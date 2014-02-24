b28dfe57539b6125f351cd0c44eb27d685e7be28
Micha Krause <micha@noris.net>
2014-02-24 16:23:19 +0100

    removed awk print statement

--- check_cdot_clusterconfig.sh
+++ check_cdot_clusterconfig.sh
@@ -10,12 +10,12 @@
 # --
 
 HOSTNAME=$1
-USERNAME=$2 
+USERNAME=$2
 
-NODES=$(ssh $USERNAME@$HOSTNAME node show | awk '$0 ~ "entries were displayed" { print $1 }')
+NODES=$(ssh $USERNAME@$HOSTNAME "node show" | awk '$0 ~ "entries were displayed" { print $1 }')
 
-VLAN_DIFF=$(ssh $USERNAME@$HOSTNAME network port vlan show | awk '$1 ~ "a0a-" { count[$1]++ } END { for (x in count) if (count[x] < '$NODES') printf x ", "; print }')
-FAILOVER_DIFF=$(ssh $USERNAME@$HOSTNAME network interface failover-groups show | awk 'BEGIN{ nodes='$NODES'; count=nodes } $1 ~ "failover" { if (nodes != count) { print group ", " }; count=0; group=substr($1, 0, length($0)-1) } $2 ~ "a0a-" {count++}')
+VLAN_DIFF=$(ssh $USERNAME@$HOSTNAME "network port vlan show" | awk '$1 ~ "a0a-" { count[$1]++ } END { for (x in count) if (count[x] < '$NODES') printf x ", " }')
+FAILOVER_DIFF=$(ssh $USERNAME@$HOSTNAME "network interface failover-groups show" | awk 'BEGIN{ nodes='$NODES'; count=nodes } $1 ~ "failover" { if (nodes != count) { print group ", " }; count=0; group=substr($1, 0, length($0)-1) } $2 ~ "a0a-" {count++}')
 
 if [ -n "$VLAN_DIFF" ] || [ -n "$FAILOVER_DIFF" ]; then {
 
@@ -26,7 +26,7 @@ if [ -n "$VLAN_DIFF" ] || [ -n "$FAILOVER_DIFF" ]; then {
         if [ -n "$FAILOVER_DIFF" ]; then
                 echo "Failover-Group configuration is different: $FAILOVER_DIFF"
         fi
-
+				exit 2
 } else {
         echo "Configuration is OK"
         exit 0
