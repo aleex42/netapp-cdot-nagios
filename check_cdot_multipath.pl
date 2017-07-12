251c97d4550949b9fe2ee7b76b424ffe164d2f49
Alexander Krogloth <root@template.noris.de>
2017-07-12 10:42:04 +0200

    fixed path count for strech/fabric metrocluster

--- check_cdot_multipath.pl
+++ check_cdot_multipath.pl
@@ -34,10 +34,35 @@ Error('Option --hostname needed!') unless $Hostname;
 Error('Option --username needed!') unless $Username;
 Error('Option --password needed!') unless $Password;
 
+my $must_paths;
+
 my $s = NaServer->new( $Hostname, 1, 3 );
 $s->set_transport_type("HTTPS");
 $s->set_style("LOGIN");
 $s->set_admin_user( $Username, $Password );
+
+# MCC check
+my $mcc_iterator = NaElement->new("metrocluster-get");
+my $mcc_response = $s->invoke_elem($mcc_iterator);
+
+my $mcc = $mcc_response->child_get("attributes");
+my $mcc_info = $mcc->child_get("metrocluster-info");
+my $config_state = $mcc_info->child_get_string("local-configuration-state");
+
+if($config_state eq "configured"){
+
+    my $type = $mcc_info->child_get_string("configuration-type");
+
+    if($type eq "stretch"){
+        $must_paths = 2;
+    } elsif($type eq "fabric"){
+        $must_paths = 8;
+    } else {
+        $must_paths = 4;
+    }
+} else {
+    $must_paths = 4;
+}
 
 my $iterator = NaElement->new("storage-disk-get-iter");
 my $tag_elem = NaElement->new("tag");
@@ -75,9 +100,7 @@ while(defined($next)){
             my $disk_path_name = $path->child_get_string("disk-name");
             my @split = split(/:/,$disk_path_name);
 
-            if((@split eq 2) && ($path_count ne 4)){
-                push @failed_disks, $disk_name;
-            } elsif((@split eq 3) && ($path_count ne 8)){
+            if((@split eq 2) && ($path_count ne $must_paths)){
                 push @failed_disks, $disk_name;
             }
         }
